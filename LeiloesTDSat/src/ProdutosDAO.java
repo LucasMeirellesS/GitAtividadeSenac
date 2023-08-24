797c17935bb1680575488592d2b7615ad87dcef0
Lucas Meirelles <lucas.meirelles_@live.com>
2023-08-24 18:08:24 -0300

    Criando a função para a atualização

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,21 +12,30 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
+    PreparedStatement st;
+    ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public int cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
+        int status;
+        try{
+            st = conn.prepareStatement("insert into filmes (nome, lancamento, categoria) values(?,?,?)");
+            st.setString(1,produto.getNome());
+            st.setString(2,produto.getData());
+            st.setString(3, produto.getCategoria());
+            status = st.executeUpdate();
+            return status;
+        }catch(SQLException ex){
+            return ex.getErrorCode();
+        }
         
     }
     
